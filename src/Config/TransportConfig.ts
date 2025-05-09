3fd18312cc6d07fb1bb060ada68c32d5604ccf44
tbressel <tbressel.dev@gmail.com>
2025-05-09 10:28:14 +0200

    🔨  create a TransportConfig class to get value easily from environnement file

---
+++ src/Config/TransportConfig.ts
@@ -0,0 +1,68 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+// define type 
+type TransportType = typeof transportTypes[number];
+
+const transportTypes = [
+  'BIKE',
+  'ELECTRIC_CAR',
+  'SCOOTER',
+  'GAS_CAR',
+  'DIESEL_VAN',
+  'TRUCK'
+] as const;
+
+
+
+
+export class TransportConfig {
+  
+  
+  /**
+   * Methode to get value from .env config file
+   * @param key 
+   * @returns 
+   */
+  private getTransportConstValue(key: string): number {
+
+    // Catch the value from env file
+    const value = process.env[key];
+    if (!value) throw new Error(`Environment variable couldn't be got: ${key}`);
+    
+    // Check if the Number value is not a NaN !
+    const parsed = Number(value);
+    if (isNaN(parsed)) throw new Error(`It must be a number: ${key}`);
+    return parsed;
+  }
+
+
+  /**
+   * Get value of emmission 
+   * @param type kind of vehicle
+   * @returns 
+   */
+  getEmissionFactor(type: TransportType): number {
+    return this.getTransportConstValue(`EMISSION_${type}`);
+  }
+
+
+  /**
+   * Get value of average speed
+   * @param type of a kind of transport
+   * @returns 
+   */
+  getAverageSpeed(type: TransportType): number {
+    return this.getTransportConstValue(`SPEED_${type}`);
+  }
+
+
+  /**
+   * Get max weight for a type of transport
+   * @param type 
+   * @returns 
+   */
+  getMaxWeight(type: TransportType): number {
+    return this.getTransportConstValue(`MAX_WEIGHT_${type}`);
+  }
+}
