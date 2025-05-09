135c8043157d634c20ee017d1bb43d92c306bd32
tbressel <tbressel.dev@gmail.com>
2025-05-09 14:51:32 +0200

    🧪  Add a calculate time estimation test  that SHOULD throw an error if distance is zero or negative

--- __tests__/unit/GreenRecommendationService.test.ts
+++ __tests__/unit/GreenRecommendationService.test.ts
@@ -24,10 +24,23 @@ describe('Testing getEmissionFactor, validateInputs and calculateEmission intro 
 
         distance = "Mouahaha" as any;
         expect(() => service.calculateTimeEstimation(distance, type)).toThrow();
-      });
-  
+    });
 
-    
+
+
+
+    it('should throw an Error if distance is 0 or negative', () => {
+        const type: TransportType = 'BIKE';
+        let distance: number = 0;
+
+        expect(() => service.calculateTimeEstimation(distance, type)).toThrow();
+
+        distance = -65;
+        expect(() => service.calculateTimeEstimation(distance, type)).toThrow();
+    });
+
+
+
 
 
 
